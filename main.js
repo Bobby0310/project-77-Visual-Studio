6c8167d2545f4cb1d2b55aa3a80b1a78acd88490
Bobby0310 <83741230+Bobby0310@users.noreply.github.com>
2021-09-15 18:21:51 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var Name="Bobby";
+
+
+var last_Name="Khatao";
+
+
+var Full_name= Name.concat (last_Name);
+console.log(Full_name);
+
+var Length=Full_name.length;
+console.log(Length);
+
+var haha="I am Jhon";
+
+
+var replace= haha.replace("Jhon","Bobby");
+console.log(replace);
+
+var a= 10;
+console.log(a);
+
+var minimum_number= Math.min(43,16,0.1);
+console.log(minimum_number);
